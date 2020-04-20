f3db7852e0f688ceabc4d15a02066ffc38f44944
colenocks <enockscoleman@gmail.com>
2020-04-20 10:06:26 +0100

    feat: add cardFlip, match and disableBoard functions

--- scripts.js
+++ scripts.js
@@ -1,17 +1,50 @@
 /* The event "DOMContentLoaded" will be fired when the document has been parsed completely, that is without stylesheets* and additional images. If you need to wait for images and stylesheets, use "load" instead. */
 
-// document.addEventListener("load", function () {
 const getAll = (selector) => document.querySelectorAll(selector);
 const get = (selector) => document.querySelector(selector);
+
+let isFlipped = false;
+let boardLocked = false;
+let firstCard, secondCard;
 
 let cards = getAll(".card");
 
 let flipCard = function () {
-  //   console.log(this);
-  this.classList.toggle("flip");
+  if (boardLocked) return;
+
+  this.classList.add("flip");
+
+  if (!isFlipped) {
+    //first click
+    isFlipped = true;
+    firstCard = this; //element that fired the event
+    return;
+  }
+  //second click
+  isFlipped = false;
+  secondCard = this;
+  checkMatch();
 };
 
-cards.forEach((card) => {
-  card.addEventListener("click", flipCard);
-});
-// });
+const checkMatch = function () {
+  firstCard.dataset.panel === secondCard.dataset.panel
+    ? disableFlip()
+    : unFlip();
+};
+
+const unFlip = function () {
+  boardLocked = true; //lock the board
+  setTimeout(function () {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    boardLocked = false; //unlock after flip
+  }, 1500);
+};
+
+const disableFlip = function () {
+  //Matched
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+};
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
